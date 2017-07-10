62715076755fe6a508acd6406788b514a7ab6e4c
bdot868 <brandoncarter100@gmail.com>
2017-07-10 16:59:49 -0700

    full react app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import './App.css'
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
+const API_KEY = 'AIzaSyCo9LMMVVYvuP3DkUyt_JVQIzfMAF35GIc';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+
+    this.videoSearch('jay z')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Youtube Mini</h2>
+      <div>
+        <div className="header">
+          <h1>Youtube Mini <span className="code">with React</span></h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar onSearchTermChange={this.videoSearch.bind(this)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
-    );
+    )
   }
 }
 
